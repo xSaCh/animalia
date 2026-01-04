ed996b7663e6a66f090337297b7b85fa3e274ba6
xSaCh <samarthchauhan7@gmail.com>
2026-01-04 14:48:23 +0530

    feat(game): basic event loop with context

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,49 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/xSaCh/animalia/internal/game"
+)
+
+const (
+	TICKS_PER_SECOND = 0.5
+)
+
 func main() {
+	milliseconds := 1000 / TICKS_PER_SECOND
+
+	ticker := time.NewTicker(time.Millisecond * time.Duration(milliseconds))
+	defer ticker.Stop()
+
+	ctx, cancel := newCtrlCContext()
+	defer cancel()
+	
+	world := game.NewWorld()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			fmt.Printf("Tick at %v\n", time.Now())
+			world.Tick()
+		}
+	}
+
+}
+
+func newCtrlCContext() (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		<-c
+		fmt.Println("Ctrl+C pressed, shutting down...")
+		cancel()
+	}()
+	return ctx, cancel
 }
